02722624a01b8c10d8bcdae45b55c0da6f9699e8
varkirsova <vvlebedeva06@gmail.com>
2025-11-29 00:25:05 +0300

    add func for all types traversals

--- binaryTree.py
+++ binaryTree.py
@@ -43,6 +43,7 @@ class BinaryTree:
         else:
             parent.right = Node(value)
 
+    #удаление
     def delete(self, value):
         node = self.root
         parent = None
@@ -96,6 +97,7 @@ class BinaryTree:
             else:
                 successor_parent.right = successor.right
 
+    # поиск минимума
     def find_min(self):
         if self.root is None:
             return None
@@ -105,6 +107,7 @@ class BinaryTree:
             node = node.left
         return node.value
 
+    # поиск максимума
     def find_max(self):
         if self.root is None:
             return None
@@ -113,3 +116,54 @@ class BinaryTree:
         while node.right:
             node = node.right
         return node.value
+
+    # прямой обход(корень, левое поддерево, правое поддерево)
+    def pre_order(self):
+        self._pre_order(self.root)
+
+    def _pre_order(self, node):
+        if node:
+            print(node.value)
+            self._pre_order(node.left)
+            self._pre_order(node.right)
+
+
+    # центрированный обход(левое поддерево, корень, правое поддерево)
+    def in_order(self):
+        self._in_order(self.root)
+
+    def _in_order(self, node):
+        if node:
+            self._pre_order(node.left)
+            print(node.value)
+            self._pre_order(node.right)
+
+    # обратный обход(левое поддерево, правое поддерево, корень)
+    def post_order(self):
+        self._post_order(self.root)
+
+    def _post_order(self, node):
+        if node:
+            self._pre_order(node.left)
+            self._pre_order(node.right)
+            print(node.value)
+
+    # обход в ширину
+    def breadth_search(self):
+        return self._breadth_search(self.root)
+
+    def _breadth_search(self, node):
+        res = []
+        if node is None:
+            return res
+        queue = [node]
+
+        while queue:
+            cur = queue.pop(0)
+            res.append(cur.value)
+            if cur.left:
+                queue.append(cur.left)
+            if cur.right:
+                queue.append(cur.right)
+
+        return res
